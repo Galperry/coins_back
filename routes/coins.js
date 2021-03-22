713d231a41087df9f79f28b8974fc2fa5c163f5e
Gal Perry <galperry2@gmail.com>
2021-03-22 19:51:42 +0200

    finished backend

---
+++ routes/coins.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+var router = express.Router();
+var coinController = require("../controllers/coinController")
+
+/* GET users listing. */
+
+router.get('/', coinController.fetchAll)
+
+router.post('/:id',coinController.addCoin)
+
+router.get('/:id',coinController.fetchCoin)
+
+router.put('/:id', coinController.updateCoin)
+
+router.delete('/:id', coinController.deleteCoin)
+
+module.exports = router;
